1fafcfc6fb30020d23628b0156fbd47e16f0f409
Zahin Harith <ZahinHarith@MSI>
2026-05-23 08:47:59 +0800

    Configure Mailgun mailer driver services

--- config/services.php
+++ config/services.php
@@ -22,6 +22,12 @@ return [
         'key' => env('RESEND_API_KEY'),
     ],
 
+    'mailgun' => [
+        'domain' => env('MAILGUN_DOMAIN'),
+        'secret' => env('MAILGUN_SECRET'),
+        'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
+    ],
+
     'ses' => [
         'key' => env('AWS_ACCESS_KEY_ID'),
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
